f1869fc6cb9ca66b00315d969cd0ea265159fa91
vais3335 <vais3335@CSLH3154.global.chainsys.com>
2022-11-17 11:55:24 +0530

    Day2 Task Completed

---
+++ Book1 - Copy.java
@@ -0,0 +1,24 @@
+package com.chainsys.day2;
+import java.util.Scanner;
+public class Book1 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		Book1 book=new Book1();
+		System.out.println("Enter book Name");
+		String bookName=sc.next();
+		System.out.println("Enter authorName");
+		String authorName=sc.next();
+		System.out.println("Enter bookNo");
+		int bookNo=12345;
+		System.out.println("Enter publishedYear");
+		int year=sc.nextInt();
+		System.out.println("Enter Rating");
+		float rating=sc.nextFloat();
+		
+		
+		
+
+	}
+
+}
